a4b64f1da826f82c18e6378e184311fc94d30269
Alexander Golubev <a.golubevspb1608@gmail.com>
2026-05-31 19:28:26 +0300

    Версия 1.1 реализация игры Wordle на Java

---
+++ src/Exception/InputFileLoaderException.java
@@ -0,0 +1,11 @@
+package Exception;
+
+public class InputFileException extends Exception{
+    public InputFileException() {
+        super();
+    }
+
+    public InputFileException(String message) {
+        super(message);
+    }
+}
---
+++ src/Exception/WordNotFoundInDictionary.java
@@ -0,0 +1,5 @@
+package Exception;
+
+public class GameException extends Exception {
+
+}
